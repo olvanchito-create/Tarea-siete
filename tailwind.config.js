0a8522345856ea5818ebf5342f001b206c6df240
olvanchito-create <olvanchito@gmail.com>
2025-09-06 16:14:43 -0600

    Archivos tarea 7

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        brand: '#1a73e8', 
+      },
+      spacing: {
+        '72': '18rem',
+      },
+      screens: {
+        'xs': '475px',
+      },
+      width: {
+        'custom-w': '36rem',
+      },
+      minWidth: {
+        'custom-min-w': '12rem',
+      },
+      minHeight: {
+        'custom-min-h': '10rem',
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+}
